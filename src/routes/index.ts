2fe6c9261fea26b2b42d9c27d91a412c1c5e2e8e
Christian Zenaty <cazenaty@yahoo.com>
2021-02-25 12:47:41 -0800

    Handle api request to serialize params to spec

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,24 @@
 import express, { Request, Response } from 'express';
 import axios from 'axios';
+import qs from 'qs';
 
 const router = express.Router();
 
 /**
  * Get all hotels
+ * Note: Nested object params need to be sent to Impala API in 
+ * bracket notation
+ * Ex. starRating: {gte: 4} ==> starRating[gte]=4
  */
 router.get('/', async (req: Request, res: Response) => {
   try {
-    const baseUrl = 'https://sandbox.impala.travel/v1/hotels?size=25&sortBy=rating:desc';
+    const baseUrl = 'https://sandbox.impala.travel/v1/hotels';
     const response = await axios.get(baseUrl, {
       headers: { 'x-api-key': process.env.HOTEL_SANDBOX_KEY },
+      params: { size: 10, sortBy: { rating: 'desc' }, ...req.query },
+      paramsSerializer: params => {        
+        return qs.stringify(params, { encode: false });
+      },
     });
     // console.log(response.data);
     res.json(response.data);
