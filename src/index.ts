5e9cef33580c6545d047b83e1fce30c54ac02991
Christian Zenaty <cazenaty@yahoo.com>
2021-03-08 15:36:34 -0800

    Add cors and i18n-iso-countries

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config();
 
 import { indexRouter } from './routes/index';
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 4000;
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 app.use(indexRouter);
