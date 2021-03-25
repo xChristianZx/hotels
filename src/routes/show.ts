6f5f1daa025668a90a3de11c486bb5bdfc2b32c1
Christian Zenaty <cazenaty@yahoo.com>
2021-03-25 10:59:10 -0700

    Add query params to show route

--- src/routes/show.ts
+++ src/routes/show.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import axios from 'axios';
+
 const router = express.Router();
 
 /**
@@ -7,14 +8,17 @@ const router = express.Router();
  * */
 router.get('/:id', async (req: Request, res: Response) => {
   const { id } = req.params;
+  const { query } = req;
+
   try {
     const baseUrl = `https://sandbox.impala.travel/v1/hotels/${id}`;
     const response = await axios.get(baseUrl, {
       headers: { 'x-api-key': process.env.HOTEL_SANDBOX_KEY },
+      params: { start: query.start, end: query.end },
     });
     res.json(response.data);
   } catch (error) {
-    console.error(error);
+    // console.error(error);
     return res.status(400).json({ message: error.message });
   }
 });
