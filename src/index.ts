c20285e5d776790b66db04d28d6a7a623de2e611
Christian Zenaty <cazenaty@yahoo.com>
2021-07-08 19:28:17 -0700

    Fix cors preflight issue

--- src/index.ts
+++ src/index.ts
@@ -21,8 +21,6 @@ export const DI = {} as {
 const PORT = process.env.PORT || 4000;
 const app = express();
 
-console.log('NODE_ENV', process.env.NODE_ENV);
-
 (async () => {
   DI.orm = await MikroORM.init(mikroConfig);
   DI.em = DI.orm.em;
@@ -39,10 +37,8 @@ console.log('NODE_ENV', process.env.NODE_ENV);
     ],
     credentials: true,
     methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    origin: (process.env.NODE_ENV === 'production'
-      ? process.env.CLIENT_PROD_DOMAIN
-      : 'http://localhost:3000'),
-    preflightContinue: false,
+    origin: [`${process.env.CLIENT_PROD_DOMAIN}`, 'http://localhost:3000'],
+    preflightContinue: true,
   };
 
   app.use(cors(corsOptions));
@@ -62,9 +58,6 @@ console.log('NODE_ENV', process.env.NODE_ENV);
 
   app.use('/hotels', hotelsRouter);
   app.use('/auth', authRouter);
-  app.use('/', (req, res) => {
-    res.send('Hello There');
-  });
 
   app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
