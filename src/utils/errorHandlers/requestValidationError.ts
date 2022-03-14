8a860111211bffb5ba6dabbcc249d1c2c9c4c2ff
Christian Zenaty <cazenaty@yahoo.com>
2022-03-14 11:48:37 -0700

    Update to handle booking api errors

--- src/utils/errorHandlers/requestValidationError.ts
+++ src/utils/errorHandlers/requestValidationError.ts
@@ -3,15 +3,19 @@ import { CustomError } from './customError';
 
 export class RequestValidationError extends CustomError {
   statusCode = 400;
-  
+
   constructor(public errors: ValidationError[]) {
     super('Invalid request parameters');
     Object.setPrototypeOf(this, RequestValidationError.prototype);
   }
-  
+
   serializeErrors() {
     return this.errors.map(err => {
-      return { message: err.msg, field: err.param };
+      if (err.hasOwnProperty('msg')) {
+        return { message: err.msg, field: err.param };
+      } else {
+        return { message: err, field: '' };
+      }
     });
   }
 }
