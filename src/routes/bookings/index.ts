7f2d4af2f3d6dfe6c109bbf750db11674312adc1
Christian Zenaty <cazenaty@yahoo.com>
2022-03-27 23:30:51 -0700

    Add cancelBookingRouter

--- src/routes/bookings/index.ts
+++ src/routes/bookings/index.ts
@@ -2,10 +2,12 @@ import express from 'express';
 
 import { newBookingRouter } from './new';
 import { userBookings } from './userBookings';
+import { cancelBookingRouter } from './cancelBooking';
 
 const router = express.Router();
 
 router.use(newBookingRouter);
 router.use(userBookings);
+router.use(cancelBookingRouter);
 
 export { router as bookingsRouter };
