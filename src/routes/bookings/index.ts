6b21ece26879395d8cd3759659c8eaccc217b2a5
Christian Zenaty <cazenaty@yahoo.com>
2023-01-04 14:24:00 -0800

    [WIP] Add updateBooking route

--- src/routes/bookings/index.ts
+++ src/routes/bookings/index.ts
@@ -2,12 +2,16 @@ import express from 'express';
 
 import { newBookingRouter } from './new';
 import { userBookingsRouter } from './userBookings';
+import { showBookingRouter } from './show';
+import { updateBookingRouter } from './update';
 import { cancelBookingRouter } from './cancelBooking';
 
 const router = express.Router();
 
 router.use(newBookingRouter);
 router.use(userBookingsRouter);
+router.use(showBookingRouter);
+router.use(updateBookingRouter);
 router.use(cancelBookingRouter);
 
 export { router as bookingsRouter };
